47c624ebe24e2bac258050cb5ffc7e90381bd50e
Bruno Vidal <brunokasprik@gmail.com>
2023-02-03 19:03:32 -0300

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import SearchParams from "./SearchParams";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Details from "./pages/Details";
 
 const App = () => {
-  
   return (
-    <div>
+    <BrowserRouter>
       <h1>Adopt Me</h1>
-      <SearchParams />
-    </div>
+      <Routes>
+        <Route path="/details/:id" element={<Details />} />
+        <Route path="/" element={<SearchParams />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
 const root = document.getElementById("root");
 
-
 createRoot(root).render(<App />);
-
-
